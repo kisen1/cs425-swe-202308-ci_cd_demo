2728be25bb071ab8a061a0ccdf490a5df6b22354
Prof OKalu <okalu1@outlook.com>
2023-07-27 15:20:27 -0500

    Added Account class

---
+++ src/main/java/edu/miu/cs/cs425/model/Account.java
@@ -0,0 +1,11 @@
+package edu.miu.cs.cs425.model;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class Account {
+    private String accountNo;
+    private double balance;
+}
